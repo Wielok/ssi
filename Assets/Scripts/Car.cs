c0de7c1539c49593168976d353ec976a3907ac4a
Wojciech Wielochowski <45285800+Wielok@users.noreply.github.com>
2020-05-24 16:52:50 +0200

    first
    first

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+   
+    [SerializeField] LayerMask SensorMask; 
+    [SerializeField] float FitnessUnchangedDie = 5;
+    
+    public static NeuralNetwork nextNetwork = new NeuralNetwork(new uint[] { 6, 5, 5, 2 }, null); 
+
+    public string id { get; private set; }
+
+    public int fitness { get; private set; }
+    public float timeAtLast { get; private set; }
+
+    public NeuralNetwork netowork { get; private set; } 
+
+    Rigidbody rigibody;
+    
+ 
+    private void Awake()
+    {
+        id = Guid.NewGuid().ToString();
+        
+        netowork = nextNetwork; 
+        nextNetwork = new NeuralNetwork(nextNetwork.Topology, null); 
+
+        rigibody = GetComponent<Rigidbody>();
+    
+
+        StartCoroutine(IsNotImproving()); 
+
+ 
+    }
+
+    private void FixedUpdate()
+    {
+       
+            float Vertical;
+            float Horizontal;
+
+            NeuralMoving(out Vertical, out Horizontal);
+
+            Move(Vertical, Horizontal);
+       
+    }
+
+    void NeuralMoving (out float Vertical, out float Horizontal)
+    {
+        double[] NeuralInput = new double[nextNetwork.Topology[0]];
+
+        NeuralInput[0] = CastRay(transform.forward, Vector3.forward, 1);
+        NeuralInput[1] = CastRay(-transform.forward, -Vector3.forward, 3);
+        NeuralInput[2] = CastRay(transform.right, Vector3.right, 5);
+        NeuralInput[3] = CastRay(-transform.right, -Vector3.right, 7);
+
+
+        float SqrtHalf = Mathf.Sqrt(0.5f);
+        NeuralInput[4] = CastRay(transform.right * SqrtHalf + transform.forward * SqrtHalf, Vector3.right * SqrtHalf + Vector3.forward * SqrtHalf, 9);
+        NeuralInput[5] = CastRay(transform.right * SqrtHalf + -transform.forward * SqrtHalf, Vector3.right * SqrtHalf + -Vector3.forward * SqrtHalf, 13);
+
+
+        double[] NeuralOutput = netowork.FeedForward(NeuralInput);
+        
+        if (NeuralOutput[0] <= 0.35f)
+            Vertical = -1;
+        else if (NeuralOutput[0] >= 0.65f)
+            Vertical = 1;
+        else
+            Vertical = 0;
+
+        if (NeuralOutput[1] <= 0.35f)
+            Horizontal = -1;
+        else if (NeuralOutput[1] >= 0.65f)
+            Horizontal = 1;
+        else
+            Horizontal = 0;
+
+        if (Vertical == 0 && Horizontal == 0)
+            Vertical = 1;
+    }
+
+    IEnumerator IsNotImproving ()
+    {
+        while(true)
+        {
+            int OldFitness = fitness; 
+            yield return new WaitForSeconds(FitnessUnchangedDie); 
+            if (OldFitness == fitness) 
+                WallHit(); 
+        }
+    }
+
+
+    double CastRay (Vector3 RayDirection, Vector3 LineDirection, int LinePositionIndex)
+    {
+        float Length = 4; 
+
+        RaycastHit Hit;
+        if (Physics.Raycast(transform.position, RayDirection, out Hit, Length, SensorMask))
+        {
+            float Dist = Vector3.Distance(Hit.point, transform.position); 
+            return Dist;
+        }
+        else
+        {
+            return Length; 
+        }
+    }
+    
+  
+    public void Move (float v, float h)
+    {
+        rigibody.velocity = transform.right * v * 4;
+        rigibody.angularVelocity = transform.up * h * 3;
+    }
+
+    
+    public void CheckpointHit ()
+    {
+        fitness++;
+        timeAtLast = Manager.Singleton.time;
+    }
+
+ 
+    public void WallHit()
+    {
+        Manager.Singleton.Dead(this, fitness); 
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Manager : MonoBehaviour
+{
+    public static Manager Singleton = null; 
+
+    [SerializeField] int CarCount = 100; 
+    [SerializeField] GameObject CarPrefab; 
+    [SerializeField] Text GenerationNumberText;
+    [SerializeField] Text sliderValue;
+    [SerializeField] Text Arrived;
+    [SerializeField] Text timer;
+    [SerializeField] Text besttimer;
+    [SerializeField] Text Arrivedinthis;
+    [SerializeField] Button start;
+    [SerializeField] Slider cutSlider;
+
+    bool end = false;
+    int GenerationCount = 0;
+    int counter = 0;
+    int counterGen = 0;
+
+    float lastbesttime=10000.0f;
+    public float time;
+    List<Car> Cars = new List<Car>(); 
+
+    NeuralNetwork BestNeuralNetwork = null; 
+    int BestFitness = -1; 
+    
+
+    private void Start()
+    {
+        start.interactable = false;
+        start.onClick.AddListener(button);
+        
+        if (Singleton == null) 
+            Singleton = this; 
+        else
+            gameObject.SetActive(false);
+
+        BestNeuralNetwork = new NeuralNetwork(Car.nextNetwork);
+        cutSlider.value = 1.0f;
+
+        StartGeneration(); 
+    }
+
+    void Update()
+    {
+        if (end != true)
+        {
+            time += Time.deltaTime;
+            Time.timeScale = cutSlider.value;
+            sliderValue.text = "Szybkość: " + cutSlider.value;
+            timer.text = time.ToString();
+        }
+    }
+    private void StartGeneration ()
+    {
+        time = 0.0f;
+        start.interactable = false;
+        counterGen = 0;
+        Arrivedinthis.text ="W tej generacji: "+ counterGen;
+        GenerationCount++;
+        GenerationNumberText.text = "Generacja: " + GenerationCount; 
+
+        for (int i = 0; i < CarCount; i++)
+        {
+            if (i == 0)
+                Car.nextNetwork = BestNeuralNetwork;
+            else
+            {
+                Car.nextNetwork = new NeuralNetwork(BestNeuralNetwork); 
+                Car.nextNetwork.Mutate();
+            }
+
+            Cars.Add(Instantiate(CarPrefab, transform.position, Quaternion.identity, transform).GetComponent<Car>()); 
+        }
+    }
+
+    public void Dead (Car DeadCar, int Fitness)
+    {
+        Cars.Remove(DeadCar); 
+        Destroy(DeadCar.gameObject); 
+
+        if (Fitness > BestFitness || (Fitness == BestFitness && DeadCar.timeAtLast<lastbesttime )) 
+        {
+            lastbesttime= DeadCar.timeAtLast;
+            BestNeuralNetwork = DeadCar.netowork; 
+            BestFitness = Fitness; 
+        }
+
+        if (Cars.Count <= 0) 
+            StartGeneration(); 
+    }
+
+
+    public void pathEnd(Car car)
+    {
+        
+        start.interactable = true;
+        counter++;
+        counterGen++;
+        Arrived.text = "Auta: " + counter;
+        Arrivedinthis.text = "W tej generacji: " + counterGen;
+        end = true;
+        Time.timeScale = 0;
+        if (lastbesttime > car.timeAtLast || besttimer.text=="Najlepszy czas: ") {
+            besttimer.text = "Naj. czas: " + car.timeAtLast; }
+    }
+    public void button()
+    {
+        end = false;
+
+    }
+}
